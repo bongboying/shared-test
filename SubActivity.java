ccb186125473ef9c144d4e9f86ed65a1e708577a
bongboying <67447622+bongboying@users.noreply.github.com>
2020-06-26 12:16:32 +0900

    dev_bongboying branch 생성 후 커밋

--- SubActivity.java
+++ SubActivity.java
@@ -2,6 +2,12 @@ public class SubActivity extends Activity {
 
     public onCreate() {
         // toDo;
+
+        // Colaborator 없이 Fork만 한 상태에서
+        // master가 생성한 branch인
+        // upstream/dev_master로 설정하여
+        // 새로운 주석을 추가함 - Push upstream 실패
+
     }
 
 }
