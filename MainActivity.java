05aca16ba6870780c2f1dc69dd54cdc88fc277c4
GSM <lmg@gsm.hs.kr>
2020-06-26 12:20:30 +0900

    Update MainActivity.java
    Master

--- MainActivity.java
+++ MainActivity.java
@@ -5,6 +5,8 @@ public class MainActivity extends Activity {
         // 1. 마스터가 본인 소유의
         // dev_maseter branch로
         // MainActivity 수정함
+
+        // 마스터 Branch MainActivity수정
     }
 
 }
