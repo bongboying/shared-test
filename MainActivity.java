6ca3e5527c81a360b5338ba7b040e33633ed3896
GSM <lmg@gsm.hs.kr>
2020-06-26 18:51:07 +0900

    asdf

--- MainActivity.java
+++ MainActivity.java
@@ -5,8 +5,6 @@ public class MainActivity extends Activity {
         // 1. 마스터가 본인 소유의
         // dev_maseter branch로
         // MainActivity 수정함
-
         // 마스터 Branch MainActivity수정
     }
-
 }
--- SubActivity.java
+++ SubActivity.java
@@ -4,6 +4,7 @@ public class SubActivity extends Activity {
         // toDo;
         // fork한 계정 소유자가 contribute한 repository에
         // 자신의 master branch를 사용하여 내용을 수정해봄.
+        // 또 수정
     }
 
 }
