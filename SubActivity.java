f99af532e4c2ce357792a93c695ceca22373e6ca
GSM <lmg@gsm.hs.kr>
2020-06-26 14:02:30 +0900

    Master Branch work

--- SubActivity.java
+++ SubActivity.java
@@ -1,14 +1,6 @@
 public class SubActivity extends Activity {
 
     public onCreate() {
-        // toDo;
-
-        // Colaborator 없이 Fork만 한 상태에서
-        // master가 생성한 branch인
-        // upstream/dev_master로 설정하여
-        // 새로운 주석을 추가함 - Push upstream 실패
-
-        // dev_master branch로 수정
     }
 
 }
