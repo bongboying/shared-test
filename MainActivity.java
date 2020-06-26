4f4d10d89964a10a31848aebb46a5dc7f2dfc9d8
GSM <lmg@gsm.hs.kr>
2020-06-26 19:02:31 +0900

    789

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,6 @@ public class MainActivity extends Activity {
 
     public onCreate() {
         // Master 소속 branch에서 작업
-        setContentView(activity_main);
         // 한번만 더 시도
         // 한 번만 더 시도
     }
