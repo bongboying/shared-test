44905fb3382d64f8b727dcce4be2ce736b304341
GSM <lmg@gsm.hs.kr>
2020-06-26 11:53:22 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,9 @@ public class MainActivity extends Activity {
 
     public onCreate() {
         // toDo;
-        // 1. 마스터가 MainActivity 수정함
+        // 1. 마스터가 본인 소유의
+        // dev_maseter branch로
+        // MainActivity 수정함
     }
 
 }
