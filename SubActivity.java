7197c698e0f322877a99de559cea902794bb6224
bongboying <67447622+bongboying@users.noreply.github.com>
2020-06-26 12:03:05 +0900

    Update SubActivity.java

--- SubActivity.java
+++ SubActivity.java
@@ -2,6 +2,8 @@ public class SubActivity extends Activity {
 
     public onCreate() {
         // toDo;
+        // fork한 계정 소유자가 contribute한 repository에
+        // 자신의 master branch를 사용하여 내용을 수정해봄.
     }
 
 }
