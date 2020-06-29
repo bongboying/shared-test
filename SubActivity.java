e8a8518c92c36f1a980b4694a4241a8e0ad67c86
GSM <lmg@gsm.hs.kr>
2020-06-29 10:02:49 +0900

    Update SubActivity.java

--- SubActivity.java
+++ SubActivity.java
@@ -2,7 +2,8 @@ public class SubActivity extends Activity {
 
     public onCreate() {
         setContentView(activity_sub);
-        // dev_bongboying branch를 통해 작업
+        // dev_bongboying branch 작업물의 수정
         // pull request를 요청함
+        // master의 dev_master branch에서 작업
     }
 }
